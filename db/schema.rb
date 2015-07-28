fb4eea527ad6d6f3db390d92ef4dbafe0199ba5d
katie Pekarek <katie_pekarek@yahoo.com>
2015-07-28 11:39:32 -0600

    reset and migrate DB

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150410181933) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "budgets", force: :cascade do |t|
     t.integer "total_due"
     t.integer "user_id"
